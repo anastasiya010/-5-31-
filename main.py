adbb416ceeccda86396fb2e371eed920f3fafe1a
anastasiya010 <144918989+anastasiya010@users.noreply.github.com>
2024-12-25 09:17:55 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,73 +1,60 @@
-#Определение классов данных
-class HardDrive:
-    def __init__(self, id, model, capacity):
-        self.id = id  # ID жесткого диска
-        self.model = model  # Модель жесткого диска
-        self.capacity = capacity  # Вместимость жесткого диска в ГБ
-
-class Computer:
-    def __init__(self, id, name):
-        self.id = id  # ID компьютера
-        self.name = name  # Название компьютера
-        self.hard_drives = []  # Список жестких дисков, связанных с компьютером
-
-    def add_hard_drive(self, hard_drive):
-        self.hard_drives.append(hard_drive)  # Добавление жесткого диска к компьютеру
+import sys
+import math
 
 
-#Создание тестовых данных
-
-# Создание жестких дисков
-hd1 = HardDrive(1, "Seagate Barracuda", 1000)
-hd2 = HardDrive(2, "Western Digital Blue", 2000)
-hd3 = HardDrive(3, "Samsung 970 EVO", 500)
-
-# Создание компьютеров
-pc1 = Computer(1, "Gaming PC")
-pc2 = Computer(2, "Office PC")
-
-# Связывание жестких дисков с компьютерами
-pc1.add_hard_drive(hd1)
-pc1.add_hard_drive(hd2)
-pc2.add_hard_drive(hd3)
+def get_coef(index, prompt):
+    try:
+        coef_str = sys.argv[index]
+    except:
+        print(prompt)
+        coef_str = input()
+    coef = float(coef_str)-9
+    return coef
 
 
-#Запрос 1: Вывод списка всех компьютеров с жесткими дисками
-def list_computers_with_hard_drives():
-    computers = [pc1, pc2]
-    print("Запрос 1: Вывод списка всех компьютеров с жесткими дисками")
-    for pc in computers:
-        print(f"Компьютер: {pc.name}")
-        for hd in pc.hard_drives:
-            print(f"  Жесткий диск: {hd.model}, Вместимость: {hd.capacity} ГБ")
-    print("\n")
+def get_roots(a, b, c):
+    result = []
+    D = b * b - 4 * a * c
+    if D == 0.0:
+        root = -b / (2.0 * a)
+        root_1 = root ** 0.5
+        root_2 = - root ** 0.5
+        result.append(root_1)
+        result.append(root_2)
+    elif D > 0.0:
+        sqD = math.sqrt(D)
+        root1 = (-b + sqD) / (2.0 * a)
+        root2 = (-b - sqD) / (2.0 * a)
+        if root1 > 0.0:
+            root_3 = root1 ** 0.5
+            root_4 = - root1 ** 0.5
+            result.append(root_3)
+            result.append(root_4)
+        elif root1 == 0.0:
+            result.append(root1)
+        if root2 > 0.0:
+            root_5 = root2 ** 0.5
+            root_6 = - root2 ** 0.5
+            result.append(root_5)
+            result.append(root_6)
+        elif root2 == 0.0:
+            result.append(root2)
+    return result
 
-list_computers_with_hard_drives()
+
+def main():
+    a = get_coef(1, 'Введите коэффициент А:')
+    b = get_coef(2, 'Введите коэффициент B:')
+    c = get_coef(3, 'Введите коэффициент C:')
+    roots = get_roots(a, b, c)
+    len_roots = len(roots)
+    if len_roots == 0:
+        print('Нет корней')
+    elif len_roots == 2:
+        print('Два корня: {} и {}'.format(roots[0],  roots[1]))
+    elif len_roots == 4:
+        print('Четыре корня: {}, {}, {} и {}'.format(roots[0], roots[1], roots[2], roots[3]))
 
 
-#Запрос 2: Вывод средней вместимости жестких дисков в каждом компьютере
-def average_capacity_per_computer():
-    computers = [pc1, pc2]
-    avg_capacities = {}
-    for pc in computers:
-        if pc.hard_drives:
-            total_capacity = sum(hd.capacity for hd in pc.hard_drives)
-            avg_capacity = round(total_capacity / len(pc.hard_drives), 2)
-            avg_capacities[pc.name] = avg_capacity
-    sorted_avg_capacities = sorted(avg_capacities.items(), key=lambda x: x[1])
-    print("Запрос 2: Вывод средней вместимости жестких дисков в каждом компьютере")
-    for name, avg in sorted_avg_capacities:
-        print(f"{name}: Средняя вместимость жестких дисков = {avg} ГБ")
-    print("\n")
-
-average_capacity_per_computer()
-
-
-#Запрос 3: Вывод всех жестких дисков, у которых вместимость больше 1000 ГБ
-def list_large_hard_drives():
-    large_drives = [hd for pc in [pc1, pc2] for hd in pc.hard_drives if hd.capacity > 1000]
-    for hd in large_drives:
-        print("Запрос 3: Вывод всех жестких дисков, у которых вместимость больше 1000 ГБ")
-        print(f"Жесткий диск: {hd.model}, Вместимость: {hd.capacity} ГБ")
-
-list_large_hard_drives()
+if __name__ == "__main__":
+    main()
